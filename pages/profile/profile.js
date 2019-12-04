4019a7e6a4f83a78642bbc7ce7e2ef17587de1da
KaiSun1990 <kai.j.sun@gmail.com>
2019-12-04 13:14:35 +0100

    gitignore

---
+++ pages/profile/profile.js
@@ -0,0 +1,84 @@
+// pages/profile/profile.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+  },
+  navigateToEditProfile: function () {
+    wx.navigateTo({
+      url: '/pages/edit_profile/edit_profile'
+    })
+  },
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    wx.BaaS.auth.getCurrentUser().then(user => {
+
+      // user 为 currentUser 
+      console.log(user)
+      console.log(user.nickname)
+      console.log(user.city)
+      console.log(user.province)
+      console.log(user.country)
+      console.log(user.gender)
+      this.setData({ user })
+    }).catch(err => {
+      // HError
+      if (err.code === 604) {
+        console.log('用户未登录')
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
